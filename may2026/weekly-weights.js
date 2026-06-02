7db962f2a8f18f68e9d54d99d4985facd3dfe798
Aaron Kahan <kahanam24@gmail.com>
2026-06-02 07:12:46 -0500

    end

--- may2026/weekly-weights.js
+++ may2026/weekly-weights.js
@@ -1,15 +1,15 @@
 (function() {
   window.WEIGHT_CHALLENGE_DATA = {
     people: [
-      { name: 'AK', weights: [187.8, 186, 187.2, null, null] },
-      { name: 'AC', weights: [185, 184.8, 184.6, null, null] },
-      { name: 'CG', weights: [273.9, 269.6, 269.2, null, null] }
+      { name: 'AK', weights: [187.8, 186, 187.2, 185.8, 188] },
+      { name: 'AC', weights: [185, 184.8, 184.6, 185, 185] },
+      { name: 'CG', weights: [273.9, 269.6, 269.2, 269.2, 272.3] }
     ],
     weeks: [
       { label: 'May 4' },
       { label: 'May 12' },
       { label: 'May 19' },
-      { label: 'May 26' },
+      { label: 'May 27' },
       { label: 'June 2' }
     ]
   };
